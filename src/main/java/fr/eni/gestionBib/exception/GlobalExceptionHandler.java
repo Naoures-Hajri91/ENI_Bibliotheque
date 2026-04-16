7918a72f243d35f601f8cb51090f32de7059122b
HendrickOS <osseux.hendrick@gmail.com>
2026-04-16 17:47:23 +0200

    Package gestion Exception

---
+++ src/main/java/fr/eni/gestionBib/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package fr.eni.gestionBib.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        // On renvoie le message de l'exception avec un code 400
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+	
+}
